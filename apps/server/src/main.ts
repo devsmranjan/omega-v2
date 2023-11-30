74f2adea3bfbc74b4d66726032286825f3876d4d
Smruti Ranjan Rana <smrutiranjan.developer@gmail.com>
2023-11-30 07:24:35 +0530

    [ADD] Middleware setup

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,24 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import express from 'express';
+import logger from 'morgan';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // urlencoded: It parses incoming requests with URL-encoded payloads and is based on body-parser. (for forms)
+app.use(logger('dev'));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello API' });
+    res.send({ message: `Hello API ${process.env.GOOGLE_ID}` });
 });
 
 app.listen(port, host, () => {
